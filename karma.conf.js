c308ab9e009db7b313311256705e5c871084c037
alexs <alexstlopes@gmail.com>
2022-09-22 22:29:24 -0300

    alterado nome do projeto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/student-app'),
+      dir: require('path').join(__dirname, './coverage/library-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'student-app'`, () => {
+  it(`should have as title 'library-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('student-app');
+    expect(app.title).toEqual('library-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('student-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('library-app app is running!');
   });
 });
