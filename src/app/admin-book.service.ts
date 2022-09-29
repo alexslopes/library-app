7349b719043e6fca0fa29d3683fc0e0f6d39269e
alexs <alexstlopes@gmail.com>
2022-09-29 19:11:40 -0300

    feito formulário para módulo

--- src/app/admin-book.service.ts
+++ src/app/admin-book.service.ts
@@ -21,9 +21,15 @@ export class AdminBookService {
     return this.http.get<any>(`${this.apiURL+"/obter-modulo-por-idioma"}/${id}?${params.toString()}`);
   }
 
-
-
   delete(book: Book) {
     return this.http.delete<any>(`${this.apiURL}/${book.id}`);
+  }
+
+  salvar(book: Book) : Observable<Book>{
+    return this.http.post<Book>(`${this.apiURL}`,book);
+  }
+
+  atualizar(book: Book) : Observable<any> {
+    return this.http.put<Book>(`${this.apiURL}/${book.id}`,book);
   }
 }
--- src/app/book/book-form/book-form.component.ts
+++ src/app/book/book-form/book-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AdminBookService } from 'src/app/admin-book.service';
+import { BookService } from 'src/app/book.service';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book-form',
@@ -7,9 +12,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+  success: boolean = false;
+  errors: String[];
+  id: number;
+
+
+  constructor(private bookService: BookService,
+    private adminService: AdminBookService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
+    this.book = new Book();
+  }
 
   ngOnInit(): void {
+    // let params: Observable<Params> = this.activatedRoute.params;
+    // params.subscribe(urlParams => {
+    //   this.id = urlParams['id'];
+    //   if (this.id) {
+    //     this.bookService.getBookById(this.id)
+    //       .subscribe(
+    //         response => this.book = response,
+    //         errorresponse => this.book = new Book())
+    //   }
+
+    // })
+
+    // console.log(this.book.description)
+  }
+
+  voltarParaListagem() {
+    this.router.navigate(['/book/lista'])
+  }
+
+  clicar() {
+    console.log(this.book);
+  }
+
+  onSubmit() {
+    let bookTosave = this.book;
+
+    if (this.id) {
+      this.adminService.atualizar(this.book)
+        .subscribe(response => {
+          this.success = true;
+          this.errors = null;
+        }, errorResponse => {
+          this.errors = ['Erro ao atualizar o módulo.']
+        })
+    } else {
+
+      this.adminService.salvar(bookTosave).subscribe(
+        (response) => {
+          this.success = true;
+          this.errors = null;
+          this.book = response;
+        }, errorResponse => {
+          this.success = false;
+          this.errors = errorResponse.error.errors;
+        })
+    }
   }
 
 }
