ebcd8c6c73abe6566b38ab2b0d04122301e06ef7
alexs <alexstlopes@gmail.com>
2022-10-13 20:50:16 -0300

    Corrigido configs para heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURLBase: 'http://localhost:8081',
+  apiURLBase: 'https://cabidiomas-library-api.herokuapp.com',
   clientId: 'library-app',
   clientSecret: '@321',
   obterTokenUrl: '/oauth/token'
